a22ffd835876a295b8cb602428f2c9053a275f62
Cffex <118915241+cffex@users.noreply.github.com>
2025-03-21 16:39:29 +0700

    fix: fixed nav bar behaving incorrectly

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_navigation_bar import st_navbar
+from streamlit_community_navigation_bar import st_navbar
 
 page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
 st.write(page)
